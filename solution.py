c3c46efb6e3d9e10aaff2a1a8d7e5e3783dac0ec
NiripsaKakVsegda <anna.poluyanenko@gmail.com>
2023-04-11 01:18:39 +0500

    Update solution.py

--- solution.py
+++ solution.py
@@ -1,10 +1,11 @@
 import pandas as pd
 import numpy as np
-from scipy.stats import ttest_ind
+from scipy.stats import mannwhitneyu
 
 chat_id = 440047378
 
 def solution(x: np.array, y: np.array) -> bool:
     alpha = 0.09
-    _, p_value = ttest_ind(x, y)
-    return p_value < alpha
+    stat, p = mannwhitneyu(x, y, alternative='greater')
+    crit = len(x) * len(y)
+    return stat > crit or p < alpha
